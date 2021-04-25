a7f171b84ea72dc59b1fb7411a7ea6567ff3f670
swg <3156201044@qq.com>
2021-04-25 20:47:46 +0800

    更新博物馆总览

--- lib/page/information/museum_list_page.dart
+++ lib/page/information/museum_list_page.dart
@@ -1,8 +1,11 @@
 ///author:swg
 ///updateDate:2021/4/23
 ///updateBy:swg
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../service/index.dart';
+import '../../models/index.dart';
 import '../../widgets/information/museum_item.dart';
 
 class MuseumListPage extends StatefulWidget {
@@ -19,14 +22,25 @@ class _MuseumListPageState extends State<MuseumListPage> {
         centerTitle: true,
       ),
       body: Container(
-        child: ListView.separated(
-            itemBuilder: (context, i) => MuseumItem(
-                  index: i,
-                ),
-            separatorBuilder: (context, i) => Divider(
-                  height: ScreenUtil().setHeight(1.0),
-                ),
-            itemCount: 10),
+        child: FutureBuilder(
+          future: MuseumBasicInformationService.getMuseumList(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<MuseumBasicInformation> list = snapshot.data;
+              return ListView.separated(
+                  itemBuilder: (context, i) => MuseumItem(
+                        information: list[i],
+                        index: i,
+                      ),
+                  separatorBuilder: (context, i) => Divider(
+                        height: ScreenUtil().setHeight(1.0),
+                      ),
+                  itemCount: list.length);
+            } else {
+              return Center();
+            }
+          },
+        ),
       ),
     );
   }
