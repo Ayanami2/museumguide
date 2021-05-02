a54870baa61f8560e48f0f652ed16530bb9169a1
ljp <1394827282@qq.com>
2021-05-02 13:25:10 +0800

    本地视频选择

--- lib/page/video_select.dart
+++ lib/page/video_select.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
+import 'package:museumguide/utils/image_util.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 
 class VideoSelectPage extends StatefulWidget {
@@ -15,6 +20,7 @@ class _VideoSelectPageState extends State<VideoSelectPage> {
   final introductionTextController = TextEditingController();
   String title;
   String introuction;
+  String username;
   bool k1=false;
   bool k2=false;
   bool k3=false;
@@ -22,15 +28,28 @@ class _VideoSelectPageState extends State<VideoSelectPage> {
   File _video;
   ImagePicker picker = ImagePicker();
   VideoPlayerController _videoPlayerController;
-
+  var thumbnailPath;
 
   _pickVideo() async {
-    print(222);
-    print(MediaQuery.of(context).size.width);
+    username="swg";
     PickedFile video =
-    await picker.getVideo(source: ImageSource.gallery);
+    await picker.getVideo(source: ImageSource.camera, maxDuration: const Duration(seconds: 10));
     _video = File(video.path);
-    //print(video);
+    print(111);
+    print(_video.path);
+    print(222);
+    thumbnailPath = await VideoThumbnail.thumbnailFile(
+        video: video.path,
+        imageFormat: ImageFormat.PNG,
+        maxWidth: 128,
+        quality: 25
+    );
+    print('thumbnailPath');
+    if(thumbnailPath==null)
+      print("kongkong");
+    else print(thumbnailPath);
+
+    print(thumbnailPath);
     _videoPlayerController = VideoPlayerController.file(_video)
       ..initialize().then((_) {
         setState(() {});
@@ -38,45 +57,89 @@ class _VideoSelectPageState extends State<VideoSelectPage> {
       });
   }
 
+  Future<void> putFileToStorage(
+      File file, String type, String name, String folder) async {
+
+    print(type);
+    final StorageReference firebaseStorageRef = FirebaseStorage.instance
+        .ref()
+        .child('$username/$folder/$name');
+    final StorageUploadTask task = firebaseStorageRef.putFile(file);
+    String ss=await firebaseStorageRef.getDownloadURL();
+    print('ss:');
+    print(ss);
+    /*
+    _firestore.collection('${user.email}' + '_' + '$toUser').add({
+      'text': 'you are sending $name ...',
+      'sender': user.email,
+      'time': DateTime.now(),
+    });
+
+*/
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
+            backgroundColor: Colors.pink[300],
             leading: IconButton(
                 icon: Icon(Icons.arrow_back),
                 onPressed: () {
                   Navigator.pop(context);
                 }),
             title: Text("视频上传"),
+            actions: [
+              FlatButton(
+                onPressed: (){
+                  _pickVideo();
+                },
+                child: Text("选择视频"),
+              ),
+            ],
           ),
           body: Column(children: [
+
             if (_video != null)
               _videoPlayerController.value.initialized
                   ? AspectRatio(
-                aspectRatio: _videoPlayerController.value.aspectRatio,
+                aspectRatio: 16.0/9.0,
                 child: VideoPlayer(_videoPlayerController),
               )
                   : Container(
               ),
 
-            Container(
-              height: ScreenUtil().setHeight(100),
-              width: ScreenUtil().setWidth(360),
-              child: FlatButton(
-                onPressed: () {
-                  _pickVideo();
-                },
-                child: Text("选择视频"),
-              ),
-            ),
+      /*      Portrait(
+            radius: 20,
+            imageProvider:
+            Image.memory(ImageUtil.base642Image(thumbnailPath))
+                .image,
+          ),
+            FutureBuilder(
+              future: thumbnailPath,
+                builder: (context,snapshot){
+                  if (!snapshot.hasData) {
+                    return Portrait(
+                      radius: 20,
+                      imageProvider: NetworkImage(
+                          'http://img8.zol.com.cn/bbs/upload/23765/23764201.jpg'),
+                    );
+                  }
+
+              return Portrait(
+                radius: 20,
+                imageProvider:
+                Image.memory(ImageUtil.base642Image(snapshot.data))
+                    .image,
+              );
+            }),*/
 
             Row(
               children: [
                 Expanded(
                   child: Container(
-                    height: ScreenUtil().setHeight(1),
+                    height: ScreenUtil().setHeight(15),
                     color: Colors.black45,
                   ),
                 )
@@ -112,6 +175,7 @@ class _VideoSelectPageState extends State<VideoSelectPage> {
               children: [
                 Expanded(
                   child: Container(
+
                     height: ScreenUtil().setHeight(1),
                     color: Colors.black45,
                   ),
@@ -155,18 +219,32 @@ class _VideoSelectPageState extends State<VideoSelectPage> {
                 )
               ],
             ),
-            Center(
+            Container(
+              color: Colors.blue,
               child: FlatButton(
-                onPressed: (){
+                onPressed: () async {
+
+                  if(_video!=null){
+                    String name=_video.path.split('/')[_video.path.split('/').length-1];
+                    print(name);
+                    String type=_video.path
+                        .split('/')[_video.path.split('/').length - 1]
+                        .split('.')[_video.path
+                        .split('/')[_video.path.split('/').length - 1]
+                        .split('.')
+                        .length -
+                        1];
+                    print(type);
+                   await FirebaseAuth.instance.signInAnonymously();
+                    await putFileToStorage(_video, type, name,'camera_video');
+                  }
                   ///上传视频
+                  ///
                 },
-                child: Container(
-                  color: Colors.blue,
-                  child: Text(
-                    "发布",
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
+                child: Text(
+                  "发布",
+                  style: TextStyle(
+                    fontSize: 40,
                   ),
                 ),
               ),
@@ -177,4 +255,17 @@ class _VideoSelectPageState extends State<VideoSelectPage> {
     );
   }
 }
+class Portrait extends StatelessWidget {
+  Portrait({this.radius, this.imageProvider});
+  final double radius;
+  final ImageProvider imageProvider;
 
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: radius,
+      foregroundColor: Colors.redAccent,
+      backgroundImage: imageProvider,
+    );
+  }
+}
